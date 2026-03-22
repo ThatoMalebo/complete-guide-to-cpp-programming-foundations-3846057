923c349ed8d4617c98f24f36fa1a7ced9c2c4a8b
ThatoMalebo <thatomalebo03@gmail.com>
2026-03-22 22:55:41 +0000

    Added a comment

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+int main(){
+    std::cout << "Hi There!" << std::endl;
+
+    std::cout << std::endl << std::endl; //put at the end of all code to separate the output and debugger
+    return 0;
+}
