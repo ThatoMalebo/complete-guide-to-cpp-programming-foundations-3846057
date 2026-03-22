3c376b24e815028ed72787e6abfd5468d13fd1e6
ThatoMalebo <thatomalebo03@gmail.com>
2026-03-22 22:56:02 +0000

    Added comments

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,12 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-
+#include <string>
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+    std::cout << "Enter your name: " << std::flush; //flush makes sure that the output is completely sent to the terminal before waiting for the user input.
+    std::cin >> name; //after running this line, the user input will be in name, cin only works for single words. name is the variable that recieves the input. std::cin >> is C++'s version of input().
+    std::cout << "Nice to meet you, " << name << "!" <<std::endl; // this sequence works as a concatenation of all the elements involved.
 
     std::cout << std::endl << std::endl;
     return 0;
