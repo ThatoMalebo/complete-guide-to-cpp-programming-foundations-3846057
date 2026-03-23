cd92f5796a471f7903a16f63bd77c7f6b420b2ec
ThatoMalebo <thatomalebo03@gmail.com>
2026-03-23 22:45:27 +0200

    added comments

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,18 @@
 
 #include <iostream>
 
+int a,b = 5; // same level of indentation as main function == means that a and b are global variables == accessable to all parts of the code
+// you can declare variables inside functions which limits their scopes with curly brackets
+//automatic/local variables == managed by the compiler and allocated in the stack segment of memory>> temporary
+
 int main(){
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout<<"a = " << a << std::endl;
+    std::cout<<"b = " << b << std::endl;
+    std::cout<<"flag = " << my_flag << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
